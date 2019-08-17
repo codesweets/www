1f3a41a34de27d028d5edb1b7578be89a6aca0be
Trevor Sundberg <tsundberg@s2.systems>
2019-08-17 14:08:47 -0700

    Updated the search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -12,24 +12,45 @@ export interface SearchProps {
 }
 
 export class Search extends React.Component<SearchProps, {} > {
+  private options: Promise<SearchOption[]>;
+
   public constructor (props: Readonly<SearchProps>) {
     super(props);
     this.state = {
       data: []
     };
+    this.options = this.populateOptions(0);
+  }
+
+  private async populateOptions (index: number) {
+    const amountPerPage = 250;
+    const searchUrl = new URL("https://registry.npmjs.com/-/v1/search");
+    searchUrl.searchParams.set("text", "keywords:codesweets-tasks");
+    searchUrl.searchParams.set("size", amountPerPage.toString());
+    searchUrl.searchParams.set("from", index.toString());
+    const response = await fetch(searchUrl.href);
+    const json = await response.json();
+    const result: SearchOption[] = json.objects.map((value: any) => ({
+      label: `${value.package.name} - ${value.package.description}`,
+      value: value.package.name
+    }));
+    if (json.total > index + amountPerPage) {
+      return result.concat(await this.populateOptions(index + amountPerPage));
+    }
+    return result;
+  }
+
+  private async loadOptions (inputValue: string) {
+    const options = await this.options;
+    return options.filter((option) => option.value.toLowerCase().includes(inputValue)).slice(0, 8);
   }
 
   public render () {
-    const searchUrl = new URL("http://registry.npmjs.com/-/v1/search");
     return <AsyncSelect
-      isSearchable={true}
-      loadOptions={async (inputValue, callback) => {
-        searchUrl.searchParams.set("text", `keywords:codesweets-tasks ${inputValue}`);
-        const response = await fetch(searchUrl.href);
-        const json = await response.json();
-        const result = json.objects.map((value: any) => ({label: value.package.name, value: value.package.name}));
-        callback(result);
-      }}
+      cacheOptions
+      defaultOptions
+      isClearable={true}
+      loadOptions={(inputValue) => this.loadOptions(inputValue)}
       onChange={this.props.onChange}
     />;
   }
